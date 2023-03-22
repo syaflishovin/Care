aa3eaf33fe7903cf5891eb4a85540e2b7c14758b
Angela <angela.christabel@gmail.com>
2023-03-22 11:40:15 +0700

    Initial Commit

---
+++ Care Watch App/CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareApp.swift
+//  Care Watch App
+//
+//  Created by Angela Christabel on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Care_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Care Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Care Watch App
+//
+//  Created by Angela Christabel on 22/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
